ee0128d384ad99d6f9501dd1becd39dc2483c59f
Vladimir Kravtsov <vladhc@gmail.com>
2019-05-18 15:50:54 +0200

    Stats logging logic moved to the Statistics class

--- p1_navigation/rl/runner.py
+++ p1_navigation/rl/runner.py
@@ -3,7 +3,6 @@ import sys
 import time
 import tensorflow as tf
 from rl import Statistics
-import math
 
 from google.cloud import storage
 
@@ -50,49 +49,8 @@ class Runner(object):
         for iteration in range(self._num_iterations):
             self._iteration = (iteration + 1)
             statistics = self._run_one_iteration()
-            self._log_experiment(statistics)
+            statistics.log()
             self._checkpoint()
-
-    def _log_experiment(self, stats):
-        self._log_scalar('episode_reward', stats.avg('episode_reward'))
-        self._log_scalar('episode_steps', stats.avg('episode_steps'))
-        self._log_scalar('training_steps', stats.avg('training_steps'))
-        self._log_scalar('training_episodes', stats.avg('training_episodes'))
-        self._log_scalar('evaluation_episodes', stats.avg('eval_episodes'))
-        self._log_scalar('replay_buffer_beta', stats.avg('replay_beta'))
-        self._log_scalar('replay_buffer_size', stats.max('replay_buffer_size'))
-        self._log_scalar('q', stats.avg('q'))
-        self._log_scalar('q_start', stats.avg('q_start'))
-        self._log_scalar(
-                'q_next_overestimate',
-                stats.avg('q_next_overestimate'))
-        self._log_scalar('q_next_err', stats.avg('q_next_err'))
-        self._log_scalar('q_next_err_std', stats.avg('q_next_err_std'))
-        self._log_scalar('loss', stats.avg('loss'))
-        self._log_scalar('epsilon', stats.avg('epsilon'))
-        self._log_scalar('steps_per_second_env', stats.rate('env_time'))
-        self._log_scalar('steps_per_second', stats.rate('step_time'))
-        self._log_scalar(
-                'steps_per_second_optimization',
-                stats.rate('optimization_time'))
-        self._log_scalar('noise_value_fc1', stats.avg('noise_value_fc1'))
-        self._log_scalar('noise_value_fc2', stats.avg('noise_value_fc2'))
-        self._log_scalar(
-                'noise_advantage_fc1',
-                stats.avg('noise_advantage_fc1'))
-        self._log_scalar(
-                'noise_advantage_fc2',
-                stats.avg('noise_advantage_fc2'))
-        self._log_scalar('noise_fc1', stats.avg('noise_fc1'))
-        self._log_scalar('noise_fc2', stats.avg('noise_fc2'))
-        self._summary_writer.flush()
-
-    def _log_scalar(self, tag, value):
-        if math.isnan(value):
-            return
-        s = tf.Summary(
-                value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self._summary_writer.add_summary(s, self._iteration)
 
     def _checkpoint(self):
         filename = '{}-{}.pth'.format(self._session_id, self._iteration)
@@ -107,7 +65,7 @@ class Runner(object):
             blob.upload_from_filename(filename=path)
 
     def _run_one_iteration(self):
-        stats = Statistics()
+        stats = Statistics(self._summary_writer, self._iteration)
 
         rewards, steps = self._run_one_phase(stats, is_training=True)
         stats.set('training_episodes', len(steps))
--- p1_navigation/rl/stats.py
+++ p1_navigation/rl/stats.py
@@ -1,11 +1,15 @@
 from collections import defaultdict
 import numpy as np
+import math
+import tensorflow as tf
 
 
 class Statistics(object):
 
-    def __init__(self):
+    def __init__(self, summary_writer, iteration):
         self._dict = defaultdict(list)
+        self._summary_writer = summary_writer
+        self._iteration = iteration
 
     def set(self, key, value):
         k = self._dict[key]
@@ -17,6 +21,8 @@ class Statistics(object):
             self.set(key, val)
 
     def avg(self, key):
+        if len(self._dict[key]) == 0:
+            return None
         return np.average(self._dict[key])
 
     def sum(self, key):
@@ -30,3 +36,44 @@ class Statistics(object):
 
     def rate(self, key):
         return self.count(key) / self.sum(key)
+
+    def log(self):
+        self._log_scalar('episode_reward', self.avg('episode_reward'))
+        self._log_scalar('episode_steps', self.avg('episode_steps'))
+        self._log_scalar('training_steps', self.avg('training_steps'))
+        self._log_scalar('training_episodes', self.avg('training_episodes'))
+        self._log_scalar('evaluation_episodes', self.avg('eval_episodes'))
+        self._log_scalar('replay_buffer_beta', self.avg('replay_beta'))
+        self._log_scalar('replay_buffer_size', self.max('replay_buffer_size'))
+        self._log_scalar('q', self.avg('q'))
+        self._log_scalar('q_start', self.avg('q_start'))
+        self._log_scalar(
+                'q_next_overestimate',
+                self.avg('q_next_overestimate'))
+        self._log_scalar('q_next_err', self.avg('q_next_err'))
+        self._log_scalar('q_next_err_std', self.avg('q_next_err_std'))
+        self._log_scalar('loss', self.avg('loss'))
+        self._log_scalar('epsilon', self.avg('epsilon'))
+        self._log_scalar('steps_per_second_env', self.rate('env_time'))
+        self._log_scalar('steps_per_second', self.rate('step_time'))
+        self._log_scalar(
+                'steps_per_second_optimization',
+                self.rate('optimization_time'))
+        self._log_scalar('noise_value_fc1', self.avg('noise_value_fc1'))
+        self._log_scalar('noise_value_fc2', self.avg('noise_value_fc2'))
+        self._log_scalar(
+                'noise_advantage_fc1',
+                self.avg('noise_advantage_fc1'))
+        self._log_scalar(
+                'noise_advantage_fc2',
+                self.avg('noise_advantage_fc2'))
+        self._log_scalar('noise_fc1', self.avg('noise_fc1'))
+        self._log_scalar('noise_fc2', self.avg('noise_fc2'))
+        self._summary_writer.flush()
+
+    def _log_scalar(self, tag, value):
+        if value is None or math.isnan(value):
+            return
+        s = tf.Summary(
+                value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        self._summary_writer.add_summary(s, self._iteration)
