d1d99485788dc19e8e8701919355d81732ba03f4
Vladimir Kravtsov <vladhc@gmail.com>
2019-05-28 21:43:56 +0200

    Updated bash script for training cartpole

--- p1_navigation/train-cartpole.sh
+++ p1_navigation/train-cartpole.sh
@@ -8,18 +8,19 @@ mkdir -p train
 STEPS=500
 EVAL_STEPS=3000
 ENV="CartPole-v1"
+EPS_DECAY=12500
 SESSION="pole"
 
 python train.py \
   --sess "$SESSION" --env "$ENV" \
   --steps $STEPS --eval_steps $EVAL_STEPS --gcp \
-  --epsilon_start 1.0 --epsilon_end 0.01 --epsilon_decay 25000 \
+  --epsilon_end 0.01 --epsilon_decay $EPS_DECAY \
   --ref_net "pole-ref" --iterations 100 --learning_rate 0.0001
 
 python train.py \
   --sess "$SESSION-every4" --env "$ENV" \
   --steps $STEPS --gcp --eval_steps $EVAL_STEPS \
-  --epsilon_start 1.0 --epsilon_end 0.01 --epsilon_decay 25000 \
+  --epsilon_end 0.01 --epsilon_decay $EPS_DECAY \
   --ref_net "pole-ref" --iterations 100 --learning_rate 0.0004 --train_freq 4
 
 python train.py --noisy \
@@ -30,25 +31,25 @@ python train.py --noisy \
 python train.py --double \
   --sess "$SESSION" --env "$ENV" \
   --steps $STEPS --eval_steps $EVAL_STEPS --gcp \
-  --epsilon_start 1.0 --epsilon_end 0.01 --epsilon_decay 25000 \
+  --epsilon_end 0.01 --epsilon_decay $EPS_DECAY \
   --ref_net "pole-ref" --iterations 100 --learning_rate 0.0001
 
 python train.py --priority \
   --sess "$SESSION" --env "$ENV" \
   --steps $STEPS --eval_steps $EVAL_STEPS --gcp \
-  --epsilon_start 1.0 --epsilon_end 0.01 --epsilon_decay 25000 \
+  --epsilon_end 0.01 --epsilon_decay $EPS_DECAY \
   --ref_net "pole-ref" --iterations 100 --learning_rate 0.0001
 
 python train.py --dueling \
   --sess "$SESSION" --env "$ENV" \
   --steps $STEPS --eval_steps $EVAL_STEPS --gcp \
-  --epsilon_start 1.0 --epsilon_end 0.01 --epsilon_decay 25000 \
+  --epsilon_end 0.01 --epsilon_decay $EPS_DECAY \
   --ref_net "pole-ref" --iterations 100 --learning_rate 0.0001
 
 python train.py --soft \
   --sess "$SESSION" --env "$ENV" \
   --steps $STEPS --eval_steps $EVAL_STEPS --gcp \
-  --epsilon_start 1.0 --epsilon_end 0.01 --epsilon_decay 25000 \
+  --epsilon_end 0.01 --epsilon_decay $EPS_DECAY \
   --ref_net "pole-ref" --iterations 100 --learning_rate 0.0001
 
 python train.py --double --dueling --noisy --priority --soft \
